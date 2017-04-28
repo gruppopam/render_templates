9807421ffa802f990803f5a7786b895cc53010a1
Chandirasekar Thiagarajan <chandru9279@gmail.com>
2017-04-28 12:46:49 +0530

    #Tech [chandrut/kishan] add a binstub

--- render_templates/lib/render_templates.rb
+++ render_templates/lib/render_templates.rb
@@ -2,13 +2,17 @@ require 'chef'
 require 'json'
 require 'fileutils'
 
-input_json = ARGV[0]
-input_template = ARGV[1]
-output_properties = ARGV[2]
+module RenderTemplates
+  def render
+    input_json = ARGV[0]
+    input_template = ARGV[1]
+    output_properties = ARGV[2]
 
 
-input_variables = JSON.parse(File.read(input_json), symbolize_names: true)
-tc = Chef::Mixin::Template::TemplateContext.new(input_variables)
-properties = tc.render_template(input_template)
+    input_variables = JSON.parse(File.read(input_json), symbolize_names: true)
+    tc = Chef::Mixin::Template::TemplateContext.new(input_variables)
+    properties = tc.render_template(input_template)
 
-File.open(output_properties, 'w') { |file| file.write(properties) }
+    File.open(output_properties, 'w') { |file| file.write(properties) }
+  end
+end
